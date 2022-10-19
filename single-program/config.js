b98773ec2a5f41fab244a437a0c3a8ad27492c73
arunsridher <arunsridher@gmail.com>
2022-10-19 13:21:43 +0530

    refactored user login code

--- single-program/config.js
+++ single-program/config.js
@@ -7,7 +7,6 @@ export function getConfig(env) {
       return {
         networkId: "mainnet",
         nodeUrl: "https://rpc.mainnet.near.org",
-        contractName: CONTRACT_NAME,
         walletUrl: "https://wallet.near.org",
         helperUrl: "https://helper.mainnet.near.org",
       };
@@ -16,7 +15,6 @@ export function getConfig(env) {
       return {
         networkId: "testnet",
         nodeUrl: "https://rpc.testnet.near.org",
-        contractName: CONTRACT_NAME,
         walletUrl: "https://wallet.testnet.near.org",
         helperUrl: "https://helper.testnet.near.org",
       };
@@ -24,7 +22,6 @@ export function getConfig(env) {
       return {
         networkId: "betanet",
         nodeUrl: "https://rpc.betanet.near.org",
-        contractName: CONTRACT_NAME,
         walletUrl: "https://wallet.betanet.near.org",
         helperUrl: "https://helper.betanet.near.org",
       };
@@ -34,21 +31,18 @@ export function getConfig(env) {
         nodeUrl: "http://localhost:3030",
         keyPath: `${process.env.HOME}/.near/validator_key.json`,
         walletUrl: "http://localhost:4000/wallet",
-        contractName: CONTRACT_NAME,
       };
     case "test":
     case "ci":
       return {
         networkId: "shared-test",
         nodeUrl: "https://rpc.ci-testnet.near.org",
-        contractName: CONTRACT_NAME,
         masterAccount: "test.near",
       };
     case "ci-betanet":
       return {
         networkId: "shared-test-staging",
         nodeUrl: "https://rpc.ci-betanet.near.org",
-        contractName: CONTRACT_NAME,
         masterAccount: "test.near",
       };
     default:
--- single-program/near-wallet.js
+++ single-program/near-wallet.js
@@ -2,10 +2,28 @@ import "regenerator-runtime/runtime";
 import { keyStores, connect, WalletConnection } from "near-api-js";
 import { APP_KEY_PREFIX, getConfig } from "./config";
 
-export async function init() {
-  const nearConfig = getConfig(process.env.NEAR_ENV || "testnet");
-  const keyStore = new keyStores.BrowserLocalStorageKeyStore();
-  const nearConnection = await connect({ keyStore, ...nearConfig });
-  const walletConnection = new WalletConnection(nearConnection, APP_KEY_PREFIX);
-  return walletConnection;
+export class Wallet {
+  wallet;
+  network;
+  constructor(network = "testnet") {
+    this.network = network;
+  }
+
+  async init() {
+    const nearConfig = getConfig(this.network);
+    const keyStore = new keyStores.BrowserLocalStorageKeyStore();
+    const nearConnection = await connect({ keyStore, ...nearConfig });
+    this.wallet = new WalletConnection(nearConnection, APP_KEY_PREFIX);
+    return this.wallet.isSignedIn();
+  }
+
+  signIn() {
+    this.wallet.requestSignIn({});
+  }
+
+  signOut() {
+    this.wallet.signOut();
+    this.wallet = this.accountId = this.createAccessKeyFor = null;
+    window.location.replace(window.location.origin + window.location.pathname);
+  }
 }

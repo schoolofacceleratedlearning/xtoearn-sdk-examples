f39227f9c5ac8c5cacffb5480aa2a97729f05979
arunsridher <arunsridher@gmail.com>
2022-10-19 16:21:28 +0530

    refactored code - integrated with updated sdk

---
+++ single-program/near-interface.js
@@ -0,0 +1,36 @@
+import { providers } from "near-api-js";
+import give_rewards from "xtoearn-near-sdk";
+
+function getConnInfo() {
+  const networkId = process.env.NEAR_ENV;
+  return { url: `https://rpc.${networkId}.near.org` };
+}
+
+async function getTransactionDetails(transactionHashes) {
+  const connInfo = getConnInfo();
+  const provider = new providers.JsonRpcProvider(connInfo);
+  const transaction = await provider.txStatus(transactionHashes, "unnused");
+  return transaction;
+}
+
+export default async function giveReward(userAccountId) {
+  try {
+    const args = {
+      reward_name: "video_likes",
+      program_name: "pe",
+      user_wallet: userAccountId,
+      program_owner: process.env.ACCOUNT_ID,
+      private_key: process.env.PRIVATE_KEY,
+    };
+    const reward = await give_rewards(
+      process.env.CONTRACT_ID,
+      process.env.ACCOUNT_ID,
+      process.env.METHOD_NAME,
+      args,
+      process.env.ATTACHED_DEPOSIT
+    );
+    return getTransactionDetails(reward.transaction.id);
+  } catch (error) {
+    console.log(error);
+  }
+}

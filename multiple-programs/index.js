51e0199ef7a0f2b4b371ad4765868e870206618a
arunsridher <arunsridher@gmail.com>
2022-10-24 16:25:37 +0530

    basic example setup

---
+++ multiple-programs/index.js
@@ -0,0 +1,84 @@
+import { Wallet } from "./near-wallet";
+import giveReward from "./near-interface";
+const wallet = new Wallet(process.env.NODE_ENV);
+
+window.onload = async () => {
+  const isSignedIn = await wallet.init();
+  if (isSignedIn) {
+    signedInFlow();
+  } else {
+    signedOutFlow();
+  }
+};
+
+// Log in and log out users using NEAR Wallet
+document.querySelector(".sign-in .btn").onclick = () => {
+  wallet.signIn();
+};
+document.querySelector(".sign-out .btn").onclick = () => {
+  wallet.signOut();
+};
+
+// Display the signed-out-flow container
+function signedOutFlow() {
+  document.querySelector(".sign-in").style.display = "block";
+}
+
+// Displaying the signed in flow container and display counter
+async function signedInFlow() {
+  document.querySelector(".sign-out").style.display = "block";
+}
+
+const hideLoadingAndShowAlert = ({
+  className,
+  message,
+  loadingElement,
+  alertElement,
+  alertTextElement,
+}) => {
+  if (loadingElement && alertElement) {
+    loadingElement.style.display = "none";
+    alertElement.style.display = "flex";
+  }
+  if (alertElement && alertTextElement) {
+    alertElement.classList.add(className);
+    alertTextElement.innerText = message;
+  }
+};
+document.querySelector("#like-video").onclick = async () => {
+  const loadingElement = document.querySelector("#loading");
+  const alertElement = document.querySelector("#alert");
+  const alertTextElement = document.querySelector("#alert-content");
+  try {
+    loadingElement.style.display = "block";
+    const transaction = await giveReward(wallet.accountId);
+    hideLoadingAndShowAlert({ loadingElement, alertElement });
+    if ("SuccessValue" in transaction?.status) {
+      hideLoadingAndShowAlert({
+        className: "alert-success",
+        message: "Reward paid out successfully!",
+        alertTextElement,
+        alertElement,
+      });
+      return;
+    }
+    hideLoadingAndShowAlert({
+      className: "alert-danger",
+      message: "Failed to pay reward!",
+      alertTextElement,
+      alertElement,
+    });
+  } catch (err) {
+    hideLoadingAndShowAlert({
+      loadingElement,
+      alertElement,
+      className: "alert-danger",
+      message: err.message,
+      alertTextElement,
+    });
+  }
+};
+
+document.querySelector("#alert-close").onclick = () => {
+  document.querySelector("#alert").style.display = "none";
+};
---
+++ multiple-programs/near-wallet.js
@@ -0,0 +1,31 @@
+import "regenerator-runtime/runtime";
+import { keyStores, connect, WalletConnection } from "near-api-js";
+import { APP_KEY_PREFIX, getConfig } from "./config";
+
+export class Wallet {
+  wallet;
+  network;
+  accountId;
+  constructor(network = "testnet") {
+    this.network = network;
+  }
+
+  async init() {
+    const nearConfig = getConfig(this.network);
+    const keyStore = new keyStores.BrowserLocalStorageKeyStore();
+    const nearConnection = await connect({ keyStore, ...nearConfig });
+    this.wallet = new WalletConnection(nearConnection, APP_KEY_PREFIX);
+    this.accountId = this.wallet.getAccountId();
+    return this.wallet.isSignedIn();
+  }
+
+  signIn() {
+    this.wallet.requestSignIn({});
+  }
+
+  signOut() {
+    this.wallet.signOut();
+    this.wallet = this.accountId = this.createAccessKeyFor = null;
+    window.location.replace(window.location.origin + window.location.pathname);
+  }
+}
